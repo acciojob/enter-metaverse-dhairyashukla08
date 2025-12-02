56f856c0eb8e3cf2fb227e04b0f2fd260cf0b507
Dhairya Shukla <113496448+dhairyashukla08@users.noreply.github.com>
2025-12-02 18:36:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,5 @@ const statusElement = document.getElementById('status');
 const enterBtn = document.getElementById('enterBtn');
 
 enterBtn.addEventListener('click', () => {
-	const newContent = document.createElement('h1');
-	newContent.textContent = "Entered Metaverse";
-	statusElement.replaceWith(newContent);
+  statusElement.innerHTML = '<h1>Entered Metaverse</h1>';
 });
