04414e2832ed99fe5403ede8994529581efa1bbe
Dhairya Shukla <113496448+dhairyashukla08@users.noreply.github.com>
2025-12-02 18:32:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const statusElement = document.getElementById('status');
+const enterBtn = document.getElementById('enterBtn');
+
+enterBtn.addEventListener('click', () => {
+	const newContent = document.createElement('h1');
+	newContent.textContent = "Entered Metaverse";
+	statusElement.replaceWith(newContent);
+});
